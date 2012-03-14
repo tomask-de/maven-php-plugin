c442dde5ff7f73d274c2d1f01a49c5467fb88c6b
edannenberg <ed@reshape.de>
2012-03-14 23:29:06 +0100

    Added phpunit.xml support for new PHPUnit impl.

--- branches/2.0-SNAPSHOT/maven-php-phpunit/src/main/java/org/phpmaven/phpunit/impl/AbstractPhpunitExeSupport.java
+++ branches/2.0-SNAPSHOT/maven-php-phpunit/src/main/java/org/phpmaven/phpunit/impl/AbstractPhpunitExeSupport.java
@@ -72,6 +72,16 @@ public abstract class AbstractPhpunitExeSupport extends AbstractPhpunitSupport {
      * @return extra arguments.
      */
     protected abstract String getExtraArguments();
+
+    /**
+     * Returns the IPhpunitTestRequest.
+     * @return IPhpunitTestRequest
+     */
+    private IPhpunitTestRequest testRequest;
+    
+    protected IPhpunitTestRequest getTestRequest() {
+        return this.testRequest;
+    }
     
     /**
      * {@inheritDoc}
@@ -80,6 +90,7 @@ public abstract class AbstractPhpunitExeSupport extends AbstractPhpunitSupport {
     public IPhpunitTestResult executeTests(IPhpunitTestRequest request, Log log)
         throws PhpException {
         try {
+            this.testRequest = request;
             final IPhpExecutable exec = this.getExec(log);
             final IPhpunitTestResult result = new PhpunitTestResult();
             
