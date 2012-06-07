53cb2bb491e6657737e63889c1c9592b4d68cc8f
Martin Eisengardt <mep_eisen@web.de>
2012-06-07 13:00:39 +0200

    moved test cases of exec into the plugin

--- branches/2.0-SNAPSHOT/maven-php-exec/src/test/java/org/phpmaven/php/test/IncludesTest.java
+++ branches/2.0-SNAPSHOT/maven-php-exec/src/test/java/org/phpmaven/php/test/IncludesTest.java
@@ -45,7 +45,7 @@ public class IncludesTest extends AbstractTestCase {
         // look up the component factory
         final IComponentFactory factory = lookup(IComponentFactory.class);
         // create the execution config
-        final MavenSession session = this.createSimpleEmptySession();
+        final MavenSession session = this.createSimpleSession("exec/empty-pom");
         final IPhpExecutableConfiguration execConfig = factory.lookup(
                 IPhpExecutableConfiguration.class,
                 IComponentFactory.EMPTY_CONFIG,
@@ -69,7 +69,7 @@ public class IncludesTest extends AbstractTestCase {
         // look up the component factory
         final IComponentFactory factory = lookup(IComponentFactory.class);
         // create the execution config
-        final MavenSession session = this.createSimpleEmptySession();
+        final MavenSession session = this.createSimpleSession("exec/empty-pom");
         final IPhpExecutableConfiguration execConfig = factory.lookup(
                 IPhpExecutableConfiguration.class,
                 IComponentFactory.EMPTY_CONFIG,
