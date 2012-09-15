f9104509ebebd68bc95c1d3d051bb101ab892372
Martin Eisengardt <mep_eisen@web.de>
2012-09-15 08:32:45 +0200

    changed to 2.0.3-SNAPSHOT

--- maven-plugins/maven-php-plugin/src/test/java/org/phpmaven/archetypes/test/LibTest.java
+++ maven-plugins/maven-php-plugin/src/test/java/org/phpmaven/archetypes/test/LibTest.java
@@ -41,7 +41,7 @@ public class LibTest extends AbstractTestCase {
         
         verifier.addCliOption("-DarchetypeGroupId=org.phpmaven");
         verifier.addCliOption("-DarchetypeArtifactId=php5-lib-archetype");
-        verifier.addCliOption("-DarchetypeVersion=2.0-SNAPSHOT");
+        verifier.addCliOption("-DarchetypeVersion=2.0.3-SNAPSHOT");
         verifier.addCliOption("-DgroupId=org.sample");
         verifier.addCliOption("-DartifactId=my-app");
         verifier.addCliOption("-Dversion=0.0.1-SNAPSHOT");
