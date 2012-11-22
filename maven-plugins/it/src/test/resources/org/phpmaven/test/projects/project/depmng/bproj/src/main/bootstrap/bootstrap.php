5bf3fed083528c32aa9e059a74e43a2146cba637
Martin Eisengardt <mep_eisen@web.de>
2012-11-22 10:01:07 +0100

    test fix

--- maven-plugins/it/src/test/resources/org/phpmaven/test/projects/project/depmng/bproj/src/main/bootstrap/bootstrap.php
+++ maven-plugins/it/src/test/resources/org/phpmaven/test/projects/project/depmng/bproj/src/main/bootstrap/bootstrap.php
@@ -1,6 +1,7 @@
 <?php 
 
 ob_start();
+mkdir($mavenDependencies['targetDir']);
 var_dump($mavenDependencies);
 file_put_contents($mavenDependencies['targetDir'].'/foo.txt', ob_get_contents());
 ob_end_clean();
